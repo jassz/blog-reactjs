edd6c2aab10971ad0a059c09ce99644dda580e96
Naja Nadhirah <najanadhirah11@gmail.com>
2023-03-07 23:45:05 +0800

    fix: change section2 data with api response, add library helper and fetcher

--- components/section2.tsx
+++ components/section2.tsx
@@ -2,46 +2,59 @@ import * as React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Author from './_child/author';
-import getPost from '../lib/helper'
+import fetcher from '../lib/fetcher'
+import Spinner from './_child/spinner';
+import Error from './_child/error';
+
 export default function section2() {
-    getPost().then(res=>console.log(res))
+    const { data, isLoading, isError } = fetcher('api/posts/post')
+
+    if(isLoading)return <Spinner />
+    if(isError)return <Error />
     return (
         <section className='container mx-auto md:px-20 py-10'>
             <h1 className='font-bold text-4xl py-12 text-center'>Latets Post</h1>
             {/* grid column */}
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-14'>
-                {Post()}
-                {Post()}
-                {Post()}
-                {Post()}
-                {Post()}
-                {Post()}
+                
+                {
+                    data.map(( value:any, index:any ) => (
+                        <Post data={value} key={index} />
+                    ))
+                }
             </div>
         </section>
     );
 }
 
-function Post() {
+function Post(data: { data: { id: any; title: any; subtitle: any; category: any; img: any; published: any; author: any; }; }) {
+    const {id, title, subtitle, category, img, published, author} = data.data
+    console.log('data', img);
+    
     return (
         <div className='item'>
             <div className='images'>
                 <Link href={"/"}>
-                    <Image src={"/images/img1.jpg"} alt="image" width={500} height={350} className="rounded" />
+                    <Image src={img || "/"} alt="image" width={500} height={350} className="rounded" />
                 </Link>
             </div>
             <div className='info flex justify-center flex-col py-4'>
                 <div className='cat'>
-                    <Link href={"/"}><span className='text-orange-600 hover:text-orange-800'>Business Travel</span></Link>
-                    <Link href={"/"}><span className='text-gray-800 hover:text-gray-600'>-July 3, 2023</span></Link>
+                    <Link href={"/"}><span className='text-orange-600 hover:text-orange-800'>{category || "Unknown"}</span></Link>
+                    <Link href={"/"}><span className='text-gray-800 hover:text-gray-600'>-{published || "Unknown"}</span></Link>
                 </div>
                 <div className='title'>
-                    <Link href={"/"}><span className='text-xl font-bold text-gray-800 hover:text-gray-600'>Your most unhappy customers are your greatest source of learning</span></Link>
+                    <Link href={"/"}>
+                        <span className='text-xl font-bold text-gray-800 hover:text-gray-600'>
+                            {title || "Title"}
+                        </span>
+                    </Link>
                 </div>
                 <p className='text-gray-500 py-3'>
-                    Unhappy customers can provide valuable feedback and insight for a business to improve its products or services. By listening to and addressing the complaints and concerns of dissatisfied customers, a company can learn what needs to be changed or improved to better meet the needs of its customers and ultimately improve its overall performance.
+                    {subtitle || "No subtitle"}
                 </p>
             </div>
-            <Author />
+            {author ? <Author /> : <></>}
         </div>
     )
 }
--- lib/helper.tsx
+++ lib/helper.tsx
@@ -1,9 +1,29 @@
 //endpoint: http://localhost:3000/api/posts
-const baseURL = "http://localhost:3000/api/posts/post"
 
-export default async function getPost(){
-    const res = await fetch(`${baseURL}`)
-    const posts = await res.json()
 
-    return posts
+import useSWR from 'swr'
+
+const baseURL = "https://dummyjson.com/"
+const response = (...args) => fetch(...args).then(res => res.json())
+
+export function getPost(endpoint:any) {
+    
+	const { data, error } = useSWR(`${baseURL}${endpoint}`, response)
+
+	return{
+		data: data,
+		isLoading: !error && !data,
+		isError: error
+	}
+}
+
+
+export function getOnePost(endpoint:any, params:any) {
+	const { data, error } = useSWR(`${baseURL}${endpoint}/${params}`, response)
+
+	return{
+		data: data,
+		isLoading: !error && !data,
+		isError: error
+	}
 }
